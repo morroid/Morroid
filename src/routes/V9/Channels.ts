e385180a4281c6cdc0430254c3bc6fd9584794f6
Skiesuwu <joshwyag@gmail.com>
2023-06-05 14:17:52 -0700

    random error idk

--- src/routes/V9/Channels.ts
+++ src/routes/V9/Channels.ts
@@ -38,34 +38,35 @@ app.get("/:channelid/messages", async (req, res) => {
     discriminator,
     public_flags,
     avatar_decoration,
-    channel_id
-  } = req.body;
-  res.json([{
-    id: req.params.channelid,
     channel_id,
-    type: 0,
-    content: "m",
-    flags: 0,
-    author: {
-      id: "m",
-      username: "m",
-      global_name: null,
-      avatar: "d0b0bb32bc2c02e8f4ecd5e6ff91c83a",
-      discriminator: "9291",
-      public_flags: 0,
-      avatar_decoration: null
+  } = req.body;
+  res.json([
+    {
+      id: req.params.channelid,
+      channel_id,
+      type: 0,
+      content: "m",
+      flags: 0,
+      author: {
+        id: "m",
+        username: "m",
+        global_name: null,
+        avatar: "d0b0bb32bc2c02e8f4ecd5e6ff91c83a",
+        discriminator: "9291",
+        public_flags: 0,
+        avatar_decoration: null,
+      },
+      attachments: [],
+      embeds: [],
+      mentions: [],
+      mention_roles: [],
+      pinned: false,
+      mention_everyone: false,
+      tts: false,
+      timestamp: "2023-06-05T20:36:35.640000+00:00",
+      edited_timestamp: null,
+      components: [],
     },
-    attachments: [],
-    embeds: [],
-    mentions: [],
-    mention_roles: [],
-    pinned: false,
-    mention_everyone: false,
-    tts: false,
-    timestamp: "2023-06-05T20:36:35.640000+00:00",
-    edited_timestamp: null,
-    flags: 0,
-    components: []
-  }]);
+  ]);
 });
 export = app;
