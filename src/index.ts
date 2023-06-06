da0d8c95c7dfb767cfc4afb10884c603ae026987
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2023-06-07 06:49:25 +1000

    m

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
 import Server from "./Server";
-import dotenv from "dotenv";
-import { resolve } from "path";
-
-const app = new Server();
-
-dotenv.config({ path: resolve(__dirname, "..", ".env") });
-
-function main() {
-  app.start();
-}
-
-main();
+(() => new Server().start())()
