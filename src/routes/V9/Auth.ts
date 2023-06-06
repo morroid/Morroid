7e8f6309c9842b9142084117932ae66e350a5586
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2023-06-07 04:07:39 +1000

    Added token logging for /register and removed await

--- src/routes/V9/Auth.ts
+++ src/routes/V9/Auth.ts
@@ -35,7 +35,8 @@ app.post("/register", async (req, res) => {
       console.log(`[ACCOUNTS]: Account Created with the Snowflake ${user.id}`);
     });
 
-    res.json({ token: await generateToken(user.id) });
+    res.json({ token: generateToken(user.id) });
+    console.log(generateToken(user.id))
   } catch (err) {
     throw err;
   }
