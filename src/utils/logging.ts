00ffb2dd00d5c2681125bec2189228e66bf25c3c
Skiesuwu <joshwyag@gmail.com>
2023-06-18 00:25:27 -0700

    changed up logging

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,16 +1,48 @@
-import chalk from "chalk";
+import chalk, { Chalk } from "chalk";
+import enviroment from "../../enviroment";
 
 export default {
-  debug(...args: unknown[]) {
-    console.log(chalk.bgGreen(" DEBUG "), ...args);
+  log(message: string): void {
+    logMessage(message, "Discord", "greenBright");
   },
-  log(...args: unknown[]) {
-    console.log(chalk.bgBlue(" INFO "), ...args);
+
+  debug(message: string): void {
+    logMessage(message, "Debug", "blueBright");
   },
-  warn(...args: unknown[]) {
-    console.log(chalk.bgRedBright(" WARN "), ...args);
+
+  error(message: string): void {
+    logMessage(message, "Error", "redBright");
   },
-  error(...args: unknown[]) {
-    console.log(chalk.bgRed(" ERROR "), ...args);
+
+  warn(message: string): void {
+    logMessage(message, "Warn", "yellowBright");
   },
 };
+
+function logMessage(message: string, type: string, color: keyof Chalk): void {
+  const timestamp = new Date().toISOString();
+  const chalkColor: any = chalk[color];
+
+  const portPattern = /(\d+)/g;
+  const Ports = `${enviroment.PORT}${enviroment.GATEWAY_PORT}`;
+  const matches = message.match(portPattern);
+
+  if (matches) {
+    const formattedMessage = matches.reduce((acc, match) => {
+      const port = parseInt(match, 10);
+      const highlightColor =
+        port === parseInt(Ports, 10) ? chalk.greenBright : chalkColor;
+      return acc.replace(match, highlightColor(match));
+    }, message);
+
+    const finalMessage = `${chalk.gray(timestamp)} [${chalkColor(
+      type
+    )}] ${formattedMessage}`;
+    console.log(finalMessage);
+  } else {
+    const formattedMessage = `${chalk.gray(timestamp)} [${chalkColor(
+      type
+    )}] ${message}`;
+    console.log(formattedMessage);
+  }
+}
